ba969c4983e7a43d5259da7e93799bb0c8790008
michelle103 <michelle_schott@gmx.de>
2021-10-03 09:34:41 +0200

    Edited login-form and added user model

---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+const validator = require('validator')
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Email is invalid!')
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    photo: {
+      type: Buffer,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
